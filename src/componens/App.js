ba28d282090a6fdd073b72dcd82d43fb268723a6
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-04-25 23:54:35 +0300

    0.0.1

---
+++ src/componens/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+    return (
+        <>
+        <h1>fadfsd sfgsdf agferg</h1>
+        </>
+    )
+}
