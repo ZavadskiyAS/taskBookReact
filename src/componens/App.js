a3809cb8c402eed118f431f9dce2b06c9343995e
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-05-04 02:23:45 +0300

    fix mod props

--- src/componens/App.js
+++ src/componens/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import css from './App.module.css'
+import statisticalData from './Statistics/statistical-data.json';
 
 export default function App() {
     return (
         <>
             <Profile/>
-            <Statistics/>
+            <Statistics title="Upload stats" stats={statisticalData}/>
         </>
     )
 }
--- src/componens/Statistics/Statistics.jsx
+++ src/componens/Statistics/Statistics.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import css from "./Statistics.module.css";
-import statisticalData from './statistical-data.json';
 
 function Statistics({title, stats}) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>Upload stats</h2>
+      <h2 className={css.title}>{title}</h2>
 
       <ul className={css.statList}>
-          {statisticalData.map(element => {
+          {stats.map(element => {
               return(
                 <li className={css.item}>
                  <span className={css.label}>{element.label}</span>
