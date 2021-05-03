86c88e9286c3417708a0ce3f70b61a8ae7105b20
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-05-04 00:37:23 +0300

    stats

--- src/componens/App.js
+++ src/componens/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import css from './App.module.css'
 
 export default function App() {
     return (
         <>
             <Profile/>
+            <Statistics/>
         </>
     )
 }
