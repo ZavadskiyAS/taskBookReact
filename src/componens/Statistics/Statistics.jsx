b317b54a5d87328d11f486f8bece6030224b7c36
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-05-04 01:57:06 +0300

    statistics

--- src/componens/Statistics/Statistics.jsx
+++ src/componens/Statistics/Statistics.jsx
@@ -4,26 +4,18 @@ import statisticalData from './statistical-data.json';
 
 function Statistics({title, stats}) {
   return (
-    <section class="statistics">
-      <h2 class="title">Upload stats</h2>
+    <section className={css.statistics}>
+      <h2 className={css.title}>Upload stats</h2>
 
-      <ul class="stat-list">
-        <li class="item">
-          <span class="label">.docx</span>
-          <span class="percentage">4%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp3</span>
-          <span class="percentage">14%</span>
-        </li>
-        <li class="item">
-          <span class="label">.pdf</span>
-          <span class="percentage">41%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp4</span>
-          <span class="percentage">12%</span>
-        </li>
+      <ul className={css.statList}>
+          {statisticalData.map(element => {
+              return(
+                <li className={css.item}>
+                 <span className={css.label}>{element.label}</span>
+                 <span className={css.percentage}>{element.percentage}</span>
+               </li>)
+          })}
+
       </ul>
     </section>
   );
