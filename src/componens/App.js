04853bedf760d121218cbcc2498cdd57390831ca
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-05-07 23:26:25 +0300

    fix

--- src/componens/App.js
+++ src/componens/App.js
@@ -5,6 +5,7 @@ import './App.module.css';
 import statisticalData from './Statistics/statistical-data.json';
 import FriendList from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
             <Profile/>
             <Statistics title="Upload stats" stats={statisticalData}/>
             <FriendList friends={friends}/>
+            <TransactionHistory items={transactions} />
         </>
     )
 }
--- src/componens/TransactionHistory/TransactionHistory.jsx
+++ src/componens/TransactionHistory/TransactionHistory.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 
-function TransactionHistory() {
+function TransactionHistory(items) {
   return (
-    <table class="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
+    items.forEach(element => {
+      
+    })
+    // <table class="transaction-history">
+    //   <thead>
+    //     <tr>
+    //       <th>Type</th>
+    //       <th>Amount</th>
+    //       <th>Currency</th>
+    //     </tr>
+    //   </thead>
 
-      <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
-        <tr>
-          <td>Withdrawal</td>
-          <td>85</td>
-          <td>USD</td>
-        </tr>
-      </tbody>
-    </table>
+    //   <tbody>
+    //     <tr>
+    //       <td>Invoice</td>
+    //       <td>125</td>
+    //       <td>USD</td>
+    //     </tr>
+    //     <tr>
+    //       <td>Withdrawal</td>
+    //       <td>85</td>
+    //       <td>USD</td>
+    //     </tr>
+    //   </tbody>
+    // </table>
   );
 }
 
