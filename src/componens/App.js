1571ce33e9322a500fc608d9d5f3cb6d845aa824
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-05-11 23:22:05 +0300

    t1 t2

--- src/componens/App.js
+++ src/componens/App.js
@@ -1,11 +1,4 @@
 import React from 'react';
-// import Profile from './Profile/Profile';
-// import Statistics from './Statistics/Statistics';
-// import statisticalData from './Statistics/statistical-data.json';
-// import FriendList from './FriendList/FriendList';
-// import friends from './FriendList/friends.json';
-// import transactions from './TransactionHistory/transactions.json';
-// import TransactionHistory from './TransactionHistory/TransactionHistory';
 import './App.module.css';
 
 export default function App() {
