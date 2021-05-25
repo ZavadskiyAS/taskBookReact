a58f288285aa9d078dff7cf1809fb27b85f265b2
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-05-25 23:34:47 +0300

    comp header

--- src/componens/App.js
+++ src/componens/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.module.css';
+import Header from './Header/Header';
 
 export default function App() {
     return (
         <>
+            <Header/>
         </>
     )
 }
