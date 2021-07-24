e1f3e698f8a6da70203bc8845436a36385672f42
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-07-24 23:08:06 +0300

    reset

--- src/componens/App.js
+++ src/componens/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.module.css';
-import Header from './Header/Header';
-import SwiperOne from './Slider/Slider';
 
 export default function App() {
     return (
         <>
-            {/* <Header/> */}
-            <SwiperOne/>
         </>
     )
 }
