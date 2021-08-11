9610c65d7d5a7a50183a7e36e66fced9820db2d7
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-08-11 23:52:48 +0300

    remove

--- src/componens/Btn.jsx
+++ src/componens/Btn.jsx
@@ -7,26 +7,11 @@ export default class Btn extends Component {
     input2: '',
     inputCheck : true,
   }
-  btn1H = e => {
-   
-     this.setState({input1: e.target.value})
-
-  }
-
-  btn2H = e => {
-    this.setState({input2: e.target.value})
-  }
-
-  btn3H = e => {
-    this.setState({inputCheck: e.target.checked})
-  }
   render() {
-    const {input1, input2, inputCheck} = this.state;
+ 
 	return (
       <div>
-          <input type="text" value={input1} onChange={this.btn1H}/>
-          <input type="phone" value={input2} onChange={this.btn2H}/>
-          <input type="checkbox" checked={inputCheck}  onChange={this.btn3H}/>
+
 	    </div>
     )
   }
